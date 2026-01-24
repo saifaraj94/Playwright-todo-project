bccc5e2b300e2ee66eb3b730f3683efa0092ee4a
Saif Araj <saifaraj94@gmail.com>
2026-01-24 14:28:49 +0200

    📦 NEW: removed the headless

--- playwright.config.ts
+++ playwright.config.ts
@@ -37,9 +37,7 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use:{
-       headless: false,
-        ...devices['Desktop Chrome'] },
+      use: {...devices['Desktop Chrome'] },
     },
 
 
